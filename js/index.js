e2e2650512e3e88b7ee2ec265eb07ffefbf56120
OUARRICH <warichsaid@gmail.com>
2016-07-09 01:03:09 +0200

    new UI with pure Css

---
+++ js/index.js
@@ -0,0 +1,26 @@
+(function(w){
+
+	w.addEventListener('load', init);
+	
+	function init(){
+		var date = new Date(),
+		currentYear = document.getElementById("currentYear");
+
+		/*Init the current year in the footer*/
+		date = date.getFullYear();
+		currentYear.innerHTML = date;
+
+		/* download Pdf and docx version of CV*/
+		$("a#cv_docx").attr({
+			target: '_blank',
+			href: '/doc/OUARRICH SAID.docx'
+		});
+		$("a#cv_pdf").attr({
+			target: '_blank',
+			href: '/doc/OUARRICH_SAID.pdf'
+		});
+
+	}
+
+
+})(this);
