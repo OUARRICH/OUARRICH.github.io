cc9eaac9b19cb3b8032b89300b6548546cd58a5a
OUARRICH <warichsaid@gmail.com>
2019-12-22 15:33:03 +0000

    feat: Add menu Items

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
+import Menu from './menu/Menu.component';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+const App = () => {
+  const [open, setOpen] = useState(false);
+  const [selectedMenuItem, setSeletctedMenuItem] = useState('EXPERIENCE');
+
+  const toogleSideBar = () => {
+    setOpen(!open);
+  };
+
+  const onClickMenuItem = (item) => {
+    setSeletctedMenuItem(item.value);
+  } 
+
+  return (
+    <div className="App">
+      <Menu onClickMenuItem={onClickMenuItem} open={open} selectedMenuItem={selectedMenuItem} />
+      <div>
+        <button className="App__barsIcon" onClick={toogleSideBar}>
+          <i className="fas fa-bars"></i>
+        </button>
+        <div className="App__Content">
+          <h1>Hi I am Said</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MediaQueryProvider } from 'react-media-query-hoc';
+
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <MediaQueryProvider>
+    <App />
+  </MediaQueryProvider>,
   document.getElementById('root')
 );
